14b9aa144fd0f06c792f5d0d15242f1049aa93c6
kinnder <kinnder@mail.ru>
2024-02-19 21:30:30 +0300

    проверка параметров запросов

--- warehouseSimulator.py
+++ warehouseSimulator.py
@@ -8,10 +8,13 @@ from urllib.parse import parse_qs, urlparse
 import cv2
 import requests
 
-activities = {'activity_101': {'video': 'Container_01_MoveToTable.mp4', 'duration': 40},
-              'activity_201': {'video': 'Container_01_MoveToRack.mp4', 'duration': 40},
-              'activity_300': {'video': 'NoOperation.mp4', 'duration': 15}
-              }
+NO_OPERATION = 'activity_0'
+
+activities = {
+    'activity_0': {'video': 'NoOperation.mp4', 'duration': 15},
+    'activity_101': {'video': 'Container_01_MoveToTable.mp4', 'duration': 40},
+    'activity_201': {'video': 'Container_01_MoveToRack.mp4', 'duration': 40}
+}
 
 
 class WebServiceHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -59,7 +62,7 @@ class WebServiceHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def _do_status(self):
-        # operation
+        # processing request
         status = self._c_status_playing if videoPlayer.is_playing else self._c_status_finished
         #
         result = {f'{self._c_status}': status}
@@ -68,11 +71,19 @@ class WebServiceHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def _do_command(self):
         parameters = parse_qs(urlparse(self.path).query)
-        operationId = int(parameters[self._c_operationId][0]) if parameters[self._c_operationId][0].isdigit() else None
-        containerId = int(parameters[self._c_containerId][0]) if parameters[self._c_containerId][0].isdigit() else None
-        # operation
-        activity_id = operationId * 100 + containerId
-        activity = f'activity_{activity_id}'
+        operationId = 0
+        if self._c_operationId in parameters.keys():
+            if parameters[self._c_operationId][0].isdigit():
+                operationId = int(parameters[self._c_operationId][0])
+        containerId = 0
+        if self._c_containerId in parameters.keys():
+            if parameters[self._c_containerId][0].isdigit():
+                containerId = int(parameters[self._c_containerId][0])
+        # processing request
+        activity = f'activity_{operationId * 100 + containerId}'
+        print(activity)
+        if activity not in activities.keys():
+            activity = NO_OPERATION
         timeEstimated = activities[activity]['duration']
         result = {f'{self._c_operationId}': operationId,
                   f'{self._c_containerId}': containerId,
@@ -81,14 +92,15 @@ class WebServiceHTTPRequestHandler(BaseHTTPRequestHandler):
         if videoPlayer.is_playing:
             result = {f'{self._c_status}': self._c_status_playing}
         else:
-            videoPlayer.show_activity(activity)
+            videoPlayer.schedule_video(activities[activity]['video'])
         #
         self._set_headers()
         self.wfile.write(json.dumps(result).encode('utf-8'))
 
 
 class WebService:
-    _serverName = '0.0.0.0'
+    # _serverName = '0.0.0.0'
+    _serverName = 'localhost'
     _serverPort = 8000
 
     def __init__(self, videoPlayer):
@@ -110,42 +122,39 @@ class WebService:
 
 
 class VideoPlayer:
-    win_name = "visualization"
+    _frameName = "visualization"
     is_playing = False
 
-    def show_activity(self, activity_name):
+    def schedule_video(self, video_name):
         with self._lock:
-            self._activity_name = activity_name
-        # logging.info("new activity_name %s", self._activity_name)
+            self._video_name = video_name
 
-    def show_video_and_wait(self, videoName: str):
+    def play_video(self):
         self.is_playing = True
-        cap = cv2.VideoCapture(videoName)
+        cap = cv2.VideoCapture(self._video_name)
+        logging.info(f'playing video: {self._video_name}')
+        logging.debug(f'{self._video_name}: started')
         while cap.isOpened():
             ret, frame = cap.read()
             if frame is None:
                 break
-            cv2.imshow(self.win_name, frame)
+            cv2.imshow(self._frameName, frame)
             cv2.waitKey(1)
-        # TODO: видеопоток должен закрываться только при открытии нового или закрытии приложения
+        logging.debug(f'{self._video_name}: finished')
         cap.release()
         self.is_playing = False
 
     _lock = threading.Lock()
-    _activity_name = None
+    _video_name = None
 
     def run(self, event):
-        cv2.namedWindow(self.win_name, cv2.WINDOW_NORMAL)
-        cv2.setWindowProperty(self.win_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.namedWindow(self._frameName, cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty(self._frameName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         logging.debug("VideoPlayer started")
-        self._activity_name = 'activity_300'
-        #        self.show_video_and_wait(activities['activity_300']['video'])
-        #        self.show_video_and_wait(activities['activity_101']['video'])
-        #        self.show_video_and_wait(activities['activity_201']['video'])
         while not event.is_set():
-            if self._activity_name is not None:
-                self.show_video_and_wait(activities[self._activity_name]['video'])
-                self._activity_name = None
+            if self._video_name is not None:
+                self.play_video()
+                self._video_name = None
             else:
                 pass
         cv2.destroyAllWindows()
@@ -157,11 +166,14 @@ class VideoPlayer:
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
-    logging.getLogger().setLevel(logging.INFO)
+    # logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger().setLevel(logging.DEBUG)
 
     videoPlayer = VideoPlayer()
     webService = WebService(videoPlayer)
     event = threading.Event()
+
+    videoPlayer.schedule_video(activities[NO_OPERATION]['video'])
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         executor.submit(webService.run, event)
@@ -169,7 +181,6 @@ if __name__ == "__main__":
         logging.info("WarehouseSimulator started")
 
         input("Press Enter to stop...\n")
-        # os.system('pause')
         event.set()
         webService.stop()
         videoPlayer.stop()
